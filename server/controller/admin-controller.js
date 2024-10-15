82ad1d3be85ecdbd7170c8df2f7708039016ba5b
Mithun Yadav <itsmithun01@gmail.com>
2024-10-15 17:12:45 +0530

    Update admin-controller.js

--- server/controller/admin-controller.js
+++ server/controller/admin-controller.js
@@ -1,28 +1,38 @@
 const User = require("../models/user-model");
 const Contact = require("../models/contact-model");
 
-
-
-const getAllUsersData = async (req, res) => {
-
+const getAllAdminsData = async (req, res, next) => {
     try {
-        const users = await User.find({}, { password: 0 });
+        const users = await User.find({ isAdmin: true }, { password: 0 });
+
+        if (!users || users.length === 0) {
+            return res.status(404).json({ message: "No Admin Found" });
+        }
+
+        return res.status(200).json(users);
+    } catch (error) {
+        next(error);
+        console.log(error);
+    }
+};
+
+const getAllUsersData = async (req, res, next) => {
+    try {
+        const users = await User.find({ isAdmin: false }, { password: 0 });
+        
         if (!users || users.length === 0) {
             return res.status(404).json({ message: "No users Exist" });
         }
 
         return res.status(200).json(users);
-
     } catch (error) {
         next(error);
         console.log(error);
-
     }
 };
 
-const getAllContactsData = async (req, res) => {
+const getAllContactsData = async (req, res, next) => {
     try {
-
         const contacts = await Contact.find();
 
         if (!contacts || contacts.length === 0) {
@@ -30,42 +40,53 @@ const getAllContactsData = async (req, res) => {
         }
 
         return res.status(200).json(contacts);
-
-
     } catch (error) {
-
+        next(error);
+        console.log(error);
     }
-}
+};
 
-const getDeleteUserById = async (req, res) => {
+const getDeleteAdminsById = async (req, res, next) => {
     try {
+        const id = req.params.id;
 
+        await User.deleteOne({ _id: id });
+
+        return res.status(200).json({ message: "Admin access revoked" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getDeleteUserById = async (req, res, next) => {
+    try {
         const id = req.params.id;
 
         await User.deleteOne({ _id: id });
 
         return res.status(200).json({ message: "User deleted successfully!" });
-
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
-
-const getDeleteContactsById = async (req, res) => {
-
+const getDeleteContactsById = async (req, res, next) => {
     try {
-
         const id = req.params.id;
 
         await Contact.deleteOne({ _id: id });
 
         return res.status(200).json({ message: "Contact deleted successfully" });
-
     } catch (error) {
         next(error);
     }
-}
+};
 
-module.exports = { getAllUsersData, getAllContactsData, getDeleteUserById, getDeleteContactsById };
-
+module.exports = { 
+    getAllAdminsData, 
+    getAllUsersData, 
+    getAllContactsData, 
+    getDeleteUserById, 
+    getDeleteContactsById, 
+    getDeleteAdminsById 
+};
