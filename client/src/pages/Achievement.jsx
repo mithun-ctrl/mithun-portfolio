240d430e5b635c35bdbf0563946aded4429844f7
Mithun Yadav <itsmithun01@gmail.com>
2024-10-15 12:57:42 +0530

    Update Achievement.jsx

--- client/src/pages/Achievement.jsx
+++ client/src/pages/Achievement.jsx
@@ -11,7 +11,7 @@ export const Achievement = () => {
                     const { achievement, description, provider, project, course, duration, certificateImage } = curElem;
 
                     // Use local file path to display the certificate
-                    const certificateImagePath = `src/assets/certificates/${certificateImage}`;
+                    const certificateImagePath = `src/images/${certificateImage}`;
 
                     return (
                         <div className="container-achievement" key={index}>
