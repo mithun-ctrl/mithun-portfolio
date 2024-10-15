bb153efd24139c0aa48ff3ac30049de05b8ad3e9
Mithun Yadav <itsmithun01@gmail.com>
2024-10-15 17:13:14 +0530

    Update admin-router.js

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -8,12 +8,21 @@ const adminMiddleware = require("../middlewares/admin-middleware");
 const router = express.Router();
 
 router
+    .route("/access")
+    .get(authMiddleware, adminMiddleware, adminController.getAllAdminsData);
+
+router
     .route("/users")
     .get(authMiddleware, adminMiddleware, adminController.getAllUsersData);
 
 router
     .route("/contacts")
     .get(authMiddleware, adminController.getAllContactsData);
+
+router
+    .route("/access/delete/:id")
+    .delete(authMiddleware, adminMiddleware, adminController.getDeleteAdminsById);
+
 
 router
     .route("/users/delete/:id")
