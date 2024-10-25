c287eaaa1c119ec2d9b2ea25b32072b707d6bfec
Mithun Yadav <itsmithun01@gmail.com>
2024-10-25 23:00:50 +0530

    Update LoginProtectedRoute.jsx
    adding logout protected route

--- client/src/pages/LoginProtectedRoute.jsx
+++ client/src/pages/LoginProtectedRoute.jsx
@@ -1,1 +1,10 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../store/auth';
 
+export const LoginProtectedRoute = () =>{
+
+    const { isLoggedIn } = useAuth();
+
+    return isLoggedIn ? <Navigate to='/' /> : <Outlet />;
+
+}
