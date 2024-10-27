f268d504ccd244672a83b0344173bf5ea5d53e75
Mithun Yadav <itsmithun01@gmail.com>
2024-10-27 20:23:13 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,61 @@
 import { NavLink } from "react-router-dom"
 import './Navbar.scss';
 import { useAuth } from "../store/auth";
+import logo from '../images/logo.webp';
+import { FaAlignLeft, FaAlignRight } from "react-icons/fa";
+import { useState } from 'react';
 
 export const Navbar = () => {
     const { isLoggedIn } = useAuth();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <>
-            <header>
-                <div id="box-navbar">
-                    <div id="logo-box">
-                        <NavLink id="logo" to="/">Mithun</NavLink>
-                    </div>
+            <div id="box-navbar" className={showMenu ? 'nav-active' : ''}>
+                <div id="logo-box">
+                    <NavLink to='/' id="logo">
+                        <img src={logo} alt="logo" />
+                    </NavLink>
+                </div>
+                
+                <div className={`nav-content ${showMenu ? 'show' : ''}`}>
                     <ul className="nav-links">
-                        <li><NavLink to="/" >Home</NavLink></li>
-                        <li><NavLink to="/achievement" >Achievement</NavLink></li>
-                        <li><NavLink to="/about" >About</NavLink></li>
-                        <li><NavLink to="/contact" >Contact</NavLink></li>
-
+                        <li><NavLink to="/" onClick={() => setShowMenu(false)}>Home</NavLink></li>
+                        <li><NavLink to="/achievement" onClick={() => setShowMenu(false)}>Achievement</NavLink></li>
+                        <li><NavLink to="/about" onClick={() => setShowMenu(false)}>About</NavLink></li>
+                        <li><NavLink to="/contact" onClick={() => setShowMenu(false)}>Contact</NavLink></li>
                     </ul>
+
                     <div id="login-reg-box">
                         <ul>
-
-                            {isLoggedIn ?(
-
-                                <li><NavLink to="/logout">Logout</NavLink></li>
-
-                            ):( <>
-
-                                    <li><NavLink to="/login">Login</NavLink></li>
-                                    <li className="register-li"><NavLink className="register" to="/register">Get Started</NavLink></li>
-
+                            {isLoggedIn ? (
+                                <li><NavLink to="/logout" onClick={() => setShowMenu(false)}>Logout</NavLink></li>
+                            ) : (
+                                <>
+                                    <li><NavLink to="/login" onClick={() => setShowMenu(false)}>Login</NavLink></li>
+                                    <li className="register-li">
+                                        <NavLink className="register" to="/register" onClick={() => setShowMenu(false)}>
+                                            Get Started
+                                        </NavLink>
+                                    </li>
                                 </>
-
                             )}
                         </ul>
                     </div>
-                    <button className="mobile-menu-btn" >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
                 </div>
-            </header>
+
+                <div className="mobile-nav-btn" onClick={toggleMenu}>
+                    {showMenu ? (
+                        <FaAlignRight className="mobile-nav-icon" />
+                    ) : (
+                        <FaAlignLeft className="mobile-nav-icon" />
+                    )}
+                </div>
+            </div>
         </>
-    )
+    );
 }
